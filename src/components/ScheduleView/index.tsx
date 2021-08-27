a9650f9157ae2dd611c9aa0dd3e9c6a5e38a1326
LucasVidigal98 <lucasvidigal3@gmail.com>
2021-08-26 23:30:04 -0300

    Add 'Imagens anexadas' text

--- src/components/ScheduleView/index.tsx
+++ src/components/ScheduleView/index.tsx
@@ -1,21 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { TimeBar, TimeBarText, ViewShape, ViewShapeContent, ViewShapeRoutine, ViewShapeText, RoutineImage } from './styles';
 import { Ionicons } from "@expo/vector-icons";
 import { AsyncStorage } from 'react-native';
-//import TemplateImages from '../../templateImage.json';
-import Image from '../../assets/icons/medicine/medicine_2.png';
 
 export interface RoutineInterface {
     hour: string;
     active: boolean;
-    medicine: string; 
-    typeMedication: string; 
+    medicine: string;
+    typeMedication: string;
     amount: number;
-    amount_dose: string; 
-    dose: string; 
-    dosage: string; 
-    routine: string; 
+    amount_dose: string;
+    dose: string;
+    dosage: string;
+    routine: string;
     observation: string;
     imgRou: string;
     imgMed: string;
@@ -26,53 +24,57 @@ interface ScheuleViewProps {
     routineObject: RoutineInterface;
 }
 
-const ScheduleView:React.FC<ScheuleViewProps> = ({ hour, routineObject }) => {
+const ScheduleView: React.FC<ScheuleViewProps> = ({ hour, routineObject }) => {
     const { navigate } = useNavigation();
-    //const [image, setImage] = useState("");
-    let image = "../../assets/icons/medicine/medicine_2.png";
-    
-    async function handleGoMedical(){
+
+    async function handleGoMedical() {
         await AsyncStorage.setItem('@mobile-med/currentHour', hour);
         navigate('Routine');
     }
 
     return (
-      <ViewShape onPress={handleGoMedical}>
-         <TimeBar>
-             <TimeBarText>{hour}</TimeBarText>
-         </TimeBar>
-        
-        {(routineObject.active) && (
-            <ViewShapeContent>
-                {routineObject.medicine !== "" && (
-                    <>
-                     <ViewShapeText>{routineObject.medicine}</ViewShapeText>
-                        <ViewShapeText>{routineObject.typeMedication}</ViewShapeText>
-                        <ViewShapeRoutine>
-                        <ViewShapeText>{`${routineObject?.amount}x`}</ViewShapeText>
-                        <ViewShapeText>{`${routineObject?.amount_dose} ${routineObject?.dose}`}</ViewShapeText>
-                    </ViewShapeRoutine>
-                    </>
-                )}
+        <ViewShape onPress={handleGoMedical}>
+            <TimeBar>
+                <TimeBarText>{hour}</TimeBarText>
+            </TimeBar>
 
-                {routineObject.routine !== "" && (
-                    <>
-                    <ViewShapeRoutine>
-                        <ViewShapeText>{(routineObject.routine === "Nenhum" ? '' : routineObject.routine)}</ViewShapeText>
-                    </ViewShapeRoutine>
+            {(routineObject.active) && (
+                <ViewShapeContent>
+                    {routineObject.medicine !== "" && (
+                        <>
+                            <ViewShapeText>{routineObject.medicine}</ViewShapeText>
+                            <ViewShapeText>{routineObject.typeMedication}</ViewShapeText>
+                            <ViewShapeRoutine>
+                                <ViewShapeText>{`${routineObject?.amount}x`}</ViewShapeText>
+                                <ViewShapeText>{`${routineObject?.amount_dose} ${routineObject?.dose}`}</ViewShapeText>
+                            </ViewShapeRoutine>
+                        </>
+                    )}
 
-                    <ViewShapeText style={{marginBottom: 0}}>{routineObject.observation}</ViewShapeText>
-                    </>
-                )}
-            </ViewShapeContent>
-        )}
+                    {routineObject.routine !== "" && (
+                        <>
+                            <ViewShapeRoutine>
+                                <ViewShapeText>{(routineObject.routine === "Nenhum" ? '' : routineObject.routine)}</ViewShapeText>
+                            </ViewShapeRoutine>
 
-        {(!routineObject.active || (routineObject.medicine === "" && routineObject.routine === "")) && (
-            <ViewShapeContent>
-                <Ionicons name="ios-add-circle" size={50} color={'#48D1CC'}/>
-            </ViewShapeContent>
-        )}
-      </ViewShape>
+                            <ViewShapeText style={{ marginBottom: 0 }}>{routineObject.observation}</ViewShapeText>
+                        </>
+                    )}
+
+                    {routineObject.imgMed.length > 0 && (
+                        <>
+                            <ViewShapeText style={{ marginBottom: 0 }}>Imagens Anexadas</ViewShapeText>
+                        </>
+                    )}
+                </ViewShapeContent>
+            )}
+
+            {(!routineObject.active || (routineObject.medicine === "" && routineObject.routine === "" && routineObject.imgMed.length === 0)) && (
+                <ViewShapeContent>
+                    <Ionicons name="ios-add-circle" size={50} color={'#48D1CC'} />
+                </ViewShapeContent>
+            )}
+        </ViewShape>
     )
 }
 
